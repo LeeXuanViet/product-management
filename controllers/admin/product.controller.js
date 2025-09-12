da7bb4e76c00f56497b97c79756290c7f821b827
Lê Xuân Việt <vietabc130@gmail.com>
2025-09-13 00:00:43 +0700

    Tối ưu phần tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
+const searchHelper = require("../../helpers/search");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -15,14 +16,10 @@ module.exports.index = async (req, res) => {
   }
   
 
-  let keyword = "";
-
-  if(req.query.keyword) {
-    keyword = req.query.keyword;
-
-    const regex = new RegExp(keyword, "i");
-
-    find.title = regex;
+  const objectSearch = searchHelper(req.query);
+  
+  if(objectSearch.regex) {
+    find.title = objectSearch.regex;
   }
 
   const products = await Product.find(find);
@@ -33,6 +30,6 @@ module.exports.index = async (req, res) => {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
-    keyword: keyword
+    keyword: objectSearch.keyword
   });
 }
